a600575eb545eb083e8c31310d3bd1de3701b59a
Oleksii Lukavnov <lukavnev@gmail.com>
2023-06-09 16:44:32 +0300

    Added NotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Outlet, RouterProvider, useLocation} from "react-router-dom";
 import {MainPage} from "./main/MainPage";
 import {BooksPage} from "./books/BooksPage";
+
+const NotFound = () => {
+    const location = useLocation();
+
+    React.useEffect(() => {
+        window.location.href = '/'; // Перенаправление на index.html
+    }, []);
+
+    return null; // Можно вернуть любой другой контент или null
+};
 
 const router = createBrowserRouter([
     {
@@ -19,10 +29,16 @@ const router = createBrowserRouter([
             {
                 path:'Books',
                 element:<BooksPage/>
-            }
+            },
+            {
+                path: '*',
+                element: <NotFound />,
+            },
         ]
     }
 ])
+
+
 
 const App = () => <RouterProvider router={router}/>
 
