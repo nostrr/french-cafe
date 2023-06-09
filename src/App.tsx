5a9094d94b4c0d12970d7f82f2af31ef73387ad2
Oleksii Lukavnov <lukavnev@gmail.com>
2023-06-09 23:51:40 +0300

    temp 23 51

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import './App.css';
 import {MainPage} from "./main/MainPage";
-// import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
-// import {BooksPage} from "./books/BooksPage";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import {BooksPage} from "./books/BooksPage";
 
-// const router = createBrowserRouter([
-//     {
-//         path: '/',
-//         element:
-//             <div className="App">
-//                 <Outlet/>
-//                 <MainPage/>
-//             </div>,
-//         // children: [
-//         //     {
-//         //         path:'Books',
-//         //         element:<BooksPage/>
-//         //     }
-//         // ]
-//     }
-// ])
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element:
+            <div className="App">
+                <Outlet/>
+            </div>,
+        children: [
+            {
+                path:'/',
+                element:  <MainPage/>
+            }
+            ,
+            {
+                path: 'Books',
+                element: <BooksPage/>
+            }
+        ]
+    }
+])
 
-const App = () => (<div className='App'>
-    <MainPage/>
-</div>)
+// const App = () => (<div className='App'>
+//     <MainPage/>
+// </div>)
 
-//const App = () => <RouterProvider router={router}/>
+const App = () => <RouterProvider router={router}/>
 
 export default App;
